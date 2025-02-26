8d35d2bd5e9c408e3639287b17980e5c4c7b428e
alkanakenan <145910279+alkanakenan@users.noreply.github.com>
2025-02-26 08:44:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ API_HASH = os.getenv("API_HASH")
 STRING_SESSION = os.getenv("STRING_SESSION")
 
 client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
-client.start()
+
+async def main():
+    print("Bot is running...")
+    await client.run_until_disconnected()  # Menjaga bot tetap berjalan
+
+with client:
+    client.loop.run_until_complete(main())
